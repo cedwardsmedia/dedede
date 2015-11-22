3f56b734b21fc1e28f97803849ae6820f637809c
Corey Edwards <corey@cedwardsmedia.com>
2015-11-22 01:31:14 -0600

    Major Fixes and Additions

--- dedede.php
+++ dedede.php
@@ -1,25 +1,57 @@
 #!/usr/bin/php
 <?php
 
-if (phpversion() < "5.3") {
+// Define values
+define(VERSION, "1.0"); // Dedede version
+define(MINPHPVER, "5.3"); // Minimum supported PHP version
+
+// Bootstrap check
+
+// Check our PHP version
+if (phpversion() < MINPHPVER) {
    echo("Kirby CMS requires PHP 5.3 or greater. You are currently running PHP " . phpversion() . ". Please upgrade PHP.\n");
    exit(1);
    }
 
+// Check for git
+if (!shell_exec("git --version")) {
+   echo "Git not found. Dedede requires Git.\nIf you know Git is installed, please verify that it is in your \$PATH.\n";
+   exit(1);
+}
+
 // Check to see if a command has been issued
+// Commands are issued in array key 1
 if ( array_key_exists ("1", $_SERVER['argv']) ) {
-   define(COMMAND, $_SERVER['argv'][1]);
+
+   // Process flags
+
+   if (in_array("--version", $_SERVER['argv']) || in_array("-v", $_SERVER['argv'])){
+      version(); // Show version information
+   } elseif (in_array("--help", $_SERVER['argv']) || in_array("-h", $_SERVER['argv'])){
+      help(); // Show help
+   } else {
+      define(COMMAND, $_SERVER['argv'][1]);
+   }
 } else {
    usage();
 }
 
 // Determine our target path
+// Paths are issued in array key 2
 if ( array_key_exists("2",$_SERVER['argv']) ) {
    $path = $_SERVER['argv'][2];
+
    if ($path[0] == DIRECTORY_SEPARATOR) {
-      define(PATH, $_SERVER['argv'][1]);
+      define(PATH, $_SERVER['argv'][2]);
+   } elseif (substr($path, 0, 2) == "./") {
+      define(PATH, getcwd() . "/" . str_replace("./", "", $path));
+   } elseif ($path == ".." || $path == "../" ) {
+      echo "Cannot install Kirby to parent directory. Target must be empty or not exist.\n";
+      exit(1);
+   } elseif (substr($path, 0, 2) == ".." && (strlen($path) > 2)) {
+      define(PATH, dirname(getcwd()) . "/" . str_replace("../", "", $_SERVER['argv'][2]));
    } else {
-      define(PATH, getcwd() . "/" . $_SERVER['argv'][1]);
+      define(PATH, getcwd() . "/" . $_SERVER['argv'][2]);
    }
 } else {
    // Get the current working directory
@@ -92,10 +124,12 @@ function doinstall(){
 
    echo "+ Success! Kirby has been installed to " . PATH . "\n";
 
+   // Check for mbstring extension in PHP (required by Kirby)
    if (!extension_loaded('mbstring')) {
-      echo "Warning: UTF-8 support in PHP not found. (mbstring extension not loaded.)";
+      echo "\nWarning: UTF-8 support in PHP not found. (mbstring extension not loaded.) Kirby will have problems with this.";
    }
 
+   exit(0);
    /* echo "Do you want to start the built-in PHP development server? [Y/N]:";
 
    $response = trim(fgets(STDIN));
@@ -137,9 +171,18 @@ function doupdate() {
       shell_exec("git submodule --quiet update panel");
    }
    echo "+ Success! Kirby has been updated at " . PATH . "\n";
+   exit(0);
 }
 
+function help() {
+   echo "Dedede is a command line tool for creating and updating Kirby CMS installations.\n\nDedede can install the latest Kirby release to a directory of your choosing by executing `dedede install /path/to/install/kirby`. Dedede will download the latest Kirby release from Github and ask if you wish to install the Kirby Panel. By using Dedede, you can remove many of the, otherwise tedious, steps involved in setting up an easily updateable Kirby installation.\n\nDedede can update any Kirby installation that was cloned from Github or created using Dedede. Dedede uses git submodules to do this.\n\nDedede is a personal project that may or may not receive new features beyond this core functionality. Dedede was crafted by Corey Edwards (@cedwardsmedia) and is licensed under the MIT License.\n";
+   exit(0);
+}
 function usage() {
-   echo "Usage goes here";
-   exit;
+   echo "Usage: dedede [command] /path/to/project\n\n  + Available commands:\n    - install => Dedede will install a fresh copy of Kirby to the specified path.\n      Example: dedede install /home/cedwardsmedia/kirby\n\n    - update => Dedede will update the existing copy of Kirby at the specified path.\n      Example: dedede update /home/cedwardsmedia/kirby-outdated\n\n  + Caveats:\n    - Dedede does not currently understand relative paths (./ ../ ~/ etc.)\n    - When installing Kirby, the path must either not exist or must be empty.\n    - Dedede can not update a non-git (or non-Dedede installed) copy of Kirby.\n\n  + Notes:\n    - Dedede officially supports Kirby 2.2+. However, it should work for all of 2.x\n";
+   exit(0);
+}
+function version() {
+   echo "Dedede v" . VERSION . "\n";
+   exit(0);
 }
